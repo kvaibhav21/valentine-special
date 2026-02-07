3d510b1de70c8c8ef958ab4413389df55d3de13a
kvaibhav21 <116862414+kvaibhav21@users.noreply.github.com>
2026-02-07 11:14:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function showSurprise() {
-    document.getElementById('surprise').style.display = 'block';
-    document.querySelector('.hero').style.display = 'none';
-}
+<script>
+    const correctPassword = "ishuvaibhav@0121"; // Updated password for Rose Day surprise
+    let enteredPassword = prompt("enter your password:");
+    if (enteredPassword !== correctPassword) {
+        alert("गलत पासवर्ड। प्रवेश निषेधित।");
+        window.location.href = "https://www.google.com";
+    }
+</script>
